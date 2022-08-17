6cacb29bae6951af3b592306addbfed8a4f18e49
Henrique Rodrigues <mandemsgs@hotmail.com>
2022-08-17 16:45:39 -0300

    First commit

---
+++ src/main/java/com/henrique/ChessGameApplication.java
@@ -0,0 +1,11 @@
+package com.henrique;
+
+public class ChessGameApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to my application!");
+
+    }
+
+}
