b06bfa98a5e0126301e5cc9a32b9f18716033747
Henrique Rodrigues <mandemsgs@hotmail.com>
2022-08-17 23:38:02 -0300

    create ChessException and ChessPosition

---
+++ src/main/java/com/henrique/chess/ChessException.java
@@ -0,0 +1,10 @@
+package com.henrique.chess;
+
+public class ChessException extends RuntimeException{
+    //private static final long serialVersionUID = 1L;
+
+    public ChessException(String message) {
+        super(message);
+    }
+
+}
